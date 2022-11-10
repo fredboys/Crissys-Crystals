b21154d29e64fb80829154421d57f05e1eae0129
fredboys <freddieboys@hotmail.co.uk>
2022-11-10 16:46:47 +0000

    Almost finished

---
+++ script.js
@@ -0,0 +1,41 @@
+const slides = document.querySelectorAll('.slides');
+const dots = document.querySelectorAll('.dot');
+const back = getElementById('backgroundText')
+const second = document.getElementsByClassName('secondText')
+const third = document.getElementsByClassName('thirdText')
+let pics = document.getElementById('image');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+let btn3 = document.querySelector('#btn3');
+
+function setActive(i){
+    for (slide of slides)
+    slide.classList.remove('active');
+    slides[i].classList.add('active')
+    for (dot of dots)
+    dot.classList.remove('active');
+    dots[i].classList.add('active');
+}
+
+for (let j = 0; j<dots.length; j++) {
+    dots[j].addEventListener('click', function(){
+        setActive(j)
+    })
+}
+
+btn1.addEventListener('click', () => {
+    pics.src = 'images/carnelian.jpg';
+    back.classList.remove('secondText', 'thirdText')
+})
+
+btn2.addEventListener('click', () => {
+    pics.src = 'images/manifest.png';
+    back.classList.remove('thirdText')
+    back.classList.add('secondText')
+})
+
+btn3.addEventListener('click', () => {
+    pics.src = 'images/tarot.png';
+    back.classList.remove('secondText')
+    back.classList.add('thirdText')
+})
